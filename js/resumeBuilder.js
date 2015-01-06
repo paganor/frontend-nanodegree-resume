47690b3306218b963dde6b73491575a51335e8c2
Rob Pagano <rob@bigcompass.com>
2015-01-06 13:00:38 -0700

    lesson 1 complete

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,4 +1,56 @@
-var awesomeThoughts = "I am Rob and I am AWESOME!";
-var funThoughts = awesomeThoughts.replace("AWESOME", "NEW");
+/*
+var formattedName = HTMLheaderName.replace("%data%", "Berto Pagano");
+var formattedRole = HTMLheaderRole.replace("%data%", "str8 baller");
+// append to header
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+*/
 
-$("#main").append(funThoughts);
+var bio = {
+	"name" : "Berto",
+	"role" : "boss of all bosses",
+	"contactInfo" : {
+		"mobile" : "555-555-5555",
+		"email" : "hoodrat@gmail.com"
+	},
+	"pictureUrl" : "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSGt5feUlzTkkNAPJU5uEk9wQ6trpDVv-T_iE3JfvmxK9av7ynz4Q",
+	"welcomeMessage" : "You think this is a motherfucking game?",
+	"skills" : ["spit hot fire", "big black man", "spit hot fire"]
+};
+
+var work = {
+	"position" : "Consultant",
+	"employer" : "nonya",
+	"years" : 3,
+	"city" : "Denver"
+};
+
+var education = {
+	"schools" : [
+		{
+			"name" : "CU - Boulder",
+			"city" : "Boulder, CO",
+			"major" : "Business",
+			"graduationYear" : 2012
+		},
+		{
+			"name" : "Fake",
+			"city" : "Fake, CO",
+			"major" : "Fake",
+			"graduationYear" : 2012
+		}
+	]
+};
+
+var project = {
+	"projects" : [
+		{
+			"name" : "Shuttle Bake-Off",
+			"year": 2012
+		},
+		{
+			"name" : "ESB Inventory",
+			"year" : 2013
+		}
+	]
+};
